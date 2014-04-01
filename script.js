a971d10cbeb147670dcb672c4a37442bc33e6647
Will Sommers <will.sommers@gmail.com>
2014-04-01 11:20:33 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// EDIT 'story' AT YOUR OWN RISK!
+var story = "It has often been said that 'a dog is a man's best NOUN_1.' " +
+    "Dogs are very ADJECTIVE_1 and can be taught many ADJECTIVE_2 tricks. " +
+    "A dog can be trained to carry a/an NOUN_2 in his mouth. " +
+    "And if you throw this NOUN_3, he will run and fetch it. " +
+    "Dogs will also bark ADVERB_1 if someone tries to break into your NOUN_4 " +
+    "during the night. One of the most popular canine pets today is the NOUN_5 " +
+    "Spaniel. Spaniels have curly COLOR coats and ADJECTIVE_3 ears. " +
+    "They also have very ADJECTIVE_4 dispositions and live to be NUMBER years old. "+
+    "Other popular dogs are ADJECTIVE_5 Terriers, German PLURAL_NOUN, and " +
+    "the ADJECTIVE_6 Poodle. Every home should have a loyal dog for a/an NOUN_6.";
+
+
+// YOUR CODE STARTS HERE! ///////////////////////////////////////
+
+// 1. Alert with a welcome message
+
+// 2. Prompt the user for each of the words and then store them
+
+// 3. Replace all placeholders words in the story
+
+// YOUR CODE ENDS HERE! /////////////////////////////////////////
+
+
+// EDIT THIS LINE AT YOUR OWN RISK!
+$(window).ready(function() {
+  displayStory(story);
+});
